55a7e8475347872cc192fc9ce725b71cbe8c6fb2
OmarElsorogy <gneomar2021@gmail.com>
2022-07-07 17:11:50 +0300

    Add destroy logic and refactor the code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,20 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
   end
 
+  def edit
+  end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was successfully created'
       redirect_to @article
@@ -19,4 +22,29 @@ class ArticlesController < ApplicationController
       render :action => 'new'
     end
   end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to @article
+    else
+      flash[:error] = 'Article could not be updated'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
